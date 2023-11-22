a321d5556d5aeea39d57b255f49d060f15988420
Tangoal <tanguylprs@gmail.com>
2023-11-22 10:46:22 +0100

    initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Hero.css'
+
+export default function Hero() {
+    return (
+        <section id="hero">
+            <div class="intro">
+                <h1>TLP Creation</h1>
+                <p>Site test</p>
+            </div>
+        </section>
+    )
+}
